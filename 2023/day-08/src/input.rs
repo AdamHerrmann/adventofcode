eddafd826f449727561c325d79c85cd46b62b027
Adam Herrmann <aherrmann@factset.com>
2023-12-09 08:38:55 -0500

    day 08

--- 2023/day-08/src/input.rs
+++ 2023/day-08/src/input.rs
@@ -1,5 +1,53 @@
-pub struct Input {}
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
-pub fn parse(_filename: &str) -> Input {
-    Input {}
+pub enum Direction {
+    Left,
+    Right,
+}
+
+pub struct Input {
+    pub directions: Vec<Direction>,
+    pub elements: Vec<String>,
+    pub left: HashMap<String, String>,
+    pub right: HashMap<String, String>,
+}
+
+impl Input {
+    pub fn parse(filename: &str) -> Input {
+        let file = File::open(filename).expect("Failed to open input file");
+        let reader = BufReader::new(file);
+        let mut lines = reader.lines();
+
+        let directions: Vec<Direction> = lines
+            .next()
+            .unwrap()
+            .unwrap()
+            .chars()
+            .map(|c| match c {
+                'L' => Direction::Left,
+                'R' => Direction::Right,
+                _ => panic!("Invalid direction"),
+            })
+            .collect();
+        let mut elements = Vec::<String>::new();
+        let mut left = HashMap::<String, String>::new();
+        let mut right = HashMap::<String, String>::new();
+
+        lines.skip(1).map(|line| line.unwrap()).for_each(|line| {
+            elements.push(String::from(&line[0..3]));
+            left.insert(String::from(&line[0..3]), String::from(&line[7..10]));
+            right.insert(String::from(&line[0..3]), String::from(&line[12..15]));
+        });
+
+        Input {
+            directions,
+            elements,
+            left,
+            right,
+        }
+    }
 }
--- 2023/day-08/src/main.rs
+++ 2023/day-08/src/main.rs
@@ -1,18 +1,101 @@
+use input::Direction;
+use num::integer;
+use std::{collections::HashSet, hash::Hash};
+
 use crate::input::Input;
 
 mod input;
 
 fn main() {
-    let input = input::parse("example.txt");
+    let input = Input::parse("input.txt");
 
     println!("Part one: {}", part_one(&input));
     println!("Part two: {}", part_two(&input));
 }
 
-fn part_one(_input: &Input) -> u32 {
-    0
+fn part_one(input: &Input) -> u32 {
+    let mut current = "AAA";
+
+    input
+        .directions
+        .iter()
+        .cycle()
+        .take_while(|direction| {
+            current = match direction {
+                Direction::Left => input.left.get(current).unwrap(),
+                Direction::Right => input.right.get(current).unwrap(),
+            };
+
+            current != "ZZZ"
+        })
+        .count() as u32
+        + 1
 }
 
-fn part_two(_input: &Input) -> u32 {
-    0
+#[derive(Eq, Clone, Copy)]
+struct State<'a> {
+    direction_number: usize,
+    label: &'a str,
+    step_number: usize,
+}
+
+impl<'a> PartialEq for State<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.direction_number == other.direction_number && self.label == other.label
+    }
+}
+
+impl<'a> Hash for State<'a> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.direction_number.hash(state);
+        self.label.hash(state);
+    }
+}
+
+// each cycle only contains one state where it stops.
+// each cycle's offset matches its loop length so we can ignore it.
+fn part_two(input: &Input) -> usize {
+    input
+        .elements
+        .iter()
+        .filter(|e| e.ends_with("A"))
+        .map(|first| {
+            let mut visited: HashSet<State> = HashSet::new();
+            let mut current = first;
+
+            let mut repeats_to = 0;
+            let repeat_at = input
+                .directions
+                .iter()
+                .enumerate()
+                .cycle()
+                .enumerate()
+                .map(|(step_number, (direction_number, direction))| {
+                    current = match direction {
+                        Direction::Left => input.left.get(current).unwrap(),
+                        Direction::Right => input.right.get(current).unwrap(),
+                    };
+
+                    State {
+                        direction_number,
+                        label: current,
+                        step_number,
+                    }
+                })
+                .take_while(|state| match visited.get(&state) {
+                    Some(state) => {
+                        repeats_to = state.step_number;
+                        false
+                    }
+                    None => {
+                        visited.insert(*state);
+                        true
+                    }
+                })
+                .count();
+
+            repeat_at - repeats_to
+        })
+        .reduce(|acc, next| integer::lcm(acc, next))
+        .unwrap()
 }
